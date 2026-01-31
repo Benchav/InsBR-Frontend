dc3bd56b6be37c68b33adeac42614b53798666cd
benchav <joshua44benja@gmail.com>
2026-01-31 10:45:33 -0600

    feat: Add service worker with Workbox for PWA features.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "/index.html",
-    "revision": "0.ram36k2d8r8"
+    "revision": "0.co26tpcabrg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
