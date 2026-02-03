d64a4cd95430c9309a8cc5d0678aea5c98a71994
benchav <joshua44benja@gmail.com>
2026-02-02 18:17:25 -0600

    feat: Implement notification system for upcoming due dates in TopBar

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "/index.html",
-    "revision": "0.s0g6uotenl8"
+    "revision": "0.i8lnd1va3lg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
